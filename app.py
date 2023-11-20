3997c3d76dce6c4eb06ce1799c1f9523483eb8e7
samushka13 <56408918+samushka13@users.noreply.github.com>
2023-11-21 00:40:32 +0200

    Moved database init to a separate file.

--- app.py
+++ app.py
@@ -4,7 +4,5 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
 
 import routes
