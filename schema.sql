1c461d8c70c0f3014016967baa9463c7b1c049d0
samushka13 <56408918+samushka13@users.noreply.github.com>
2023-11-20 22:14:37 +0200

    Added tentative schema.

---
+++ schema.sql
@@ -0,0 +1,61 @@
+CREATE TYPE gender AS ENUM ("female", "male", "other");
+CREATE TYPE credentials AS VARCHAR (50);
+CREATE TYPE title AS VARCHAR (100);
+CREATE TYPE bodytext AS VARCHAR (1000);
+CREATE TYPE zipcode AS CHAR (5);
+CREATE TYPE status AS ENUM ("read", "wip", "completed");
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name CREDENTIALS UNIQUE,
+    password CREDENTIALS,
+    date_of_birth DATE,
+    gender GENDER,
+    zip_code ZIPCODE,
+    admin BOOLEAN,
+);
+
+CREATE TABLE news (
+    id SERIAL PRIMARY KEY,
+    title TITLE,
+    body BODYTEXT,
+    zip_code ZIPCODE,
+    publish_on DATETIME,
+    archive_on DATETIME,
+    created_at TIMESTAMP,
+    created_by INTEGER REFERENCES users,
+    views INTEGER,
+);
+
+CREATE TABLE notices (
+    id SERIAL PRIMARY KEY,
+    title TITLE,
+    body BODYTEXT,
+    zip_code ZIPCODE,
+    street_address TITLE,
+    status STATUS,
+    created_at TIMESTAMP,
+    created_by INTEGER REFERENCES users,
+    views INTEGER,
+);
+
+CREATE TABLE polls (
+    id SERIAL PRIMARY KEY,
+    title TITLE,
+    zip_code ZIPCODE,
+    open_on DATETIME,
+    close_on DATETIME,
+    created_at TIMESTAMP,
+    created_by INTEGER REFERENCES users,
+    thumbs_up_by INTEGER REFERENCES users [],
+    thumbs_down_by INTEGER REFERENCES users [],
+);
+
+CREATE TABLE feedbacks (
+    id SERIAL PRIMARY KEY,
+    title TITLE,
+    body BODYTEXT,
+    sent_at TIMESTAMP,
+    sent_by INTEGER REFERENCES users,
+    acknowledged BOOLEAN,
+);
