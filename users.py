84acd7cc4f804cff1b9ca9b8dda365a8195f8fe7
samushka13 <56408918+samushka13@users.noreply.github.com>
2023-11-21 19:12:38 +0200

    Specified argument types in the registration logic.

--- users.py
+++ users.py
@@ -4,7 +4,7 @@ from sqlalchemy.sql import text
 from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
 
-def register(name, password, date_of_birth, gender, zip_code, admin):
+def register(name: str, password: str, date_of_birth: str, gender: str, zip_code: str, admin: bool):
     hash_value = generate_password_hash(password)
 
     try:
@@ -12,12 +12,12 @@ def register(name, password, date_of_birth, gender, zip_code, admin):
                  VALUES (:name, :password, :date_of_birth, :gender, :zip_code, :admin)"""
 
         new_user = {
-            "name":name,
-            "password":hash_value,
-            "date_of_birth":date_of_birth,
-            "gender":gender,
-            "zip_code":zip_code,
-            "admin":admin
+            "name": name,
+            "password": hash_value,
+            "date_of_birth": date_of_birth,
+            "gender": gender,
+            "zip_code": zip_code,
+            "admin": admin
         }
 
         db.session.execute(text(sql), new_user)
