7511ee125df53a28bea3d5572c65985b8499d817
samushka13 <56408918+samushka13@users.noreply.github.com>
2023-11-23 19:02:11 +0200

    Updated schema with more precise values about who does what and when.

--- schema.sql
+++ schema.sql
@@ -10,8 +10,8 @@ DROP TABLE IF EXISTS polls CASCADE;
 DROP TABLE IF EXISTS votes CASCADE;
 DROP TABLE IF EXISTS feedbacks CASCADE;
 
-CREATE TYPE gender AS ENUM ('female', 'male', 'other');
-CREATE TYPE status AS ENUM ('read', 'wip', 'completed');
+CREATE TYPE gender AS ENUM ("female", "male", "other");
+CREATE TYPE status AS ENUM ("read", "wip", "done");
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
@@ -21,7 +21,8 @@ CREATE TABLE users (
     gender GENDER,
     zip_code CHAR(5),
     admin BOOLEAN,
-    disabled BOOLEAN
+    disabled_at TIMESTAMP,
+    disabled_by INTEGER REFERENCES users,
 );
 
 CREATE TABLE news (
@@ -31,7 +32,7 @@ CREATE TABLE news (
     zip_code CHAR(5),
     publish_on DATE,
     created_at TIMESTAMP,
-    created_by INTEGER REFERENCES users
+    created_by INTEGER REFERENCES users,
     archived_at TIMESTAMP,
     archived_by INTEGER REFERENCES users
 );
@@ -49,21 +50,31 @@ CREATE TABLE notices (
     body VARCHAR(1000),
     zip_code CHAR(5),
     street_address VARCHAR(100),
-    status STATUS,
     created_at TIMESTAMP,
-    created_by INTEGER REFERENCES users
+    created_by INTEGER REFERENCES users,
+    archived_at TIMESTAMP,
+    archived_by INTEGER REFERENCES users
 );
 
 CREATE TABLE notice_views (
     id SERIAL PRIMARY KEY,
     notice_id INTEGER REFERENCES notices,
+    viewed_at TIMESTAMP,
     viewed_by INTEGER REFERENCES users
 );
 
 CREATE TABLE notice_supports (
     id SERIAL PRIMARY KEY,
     notice_id INTEGER REFERENCES notices,
+    supported_at TIMESTAMP,
     supported_by INTEGER REFERENCES users
+);
+
+CREATE TABLE notice_statuses (
+    id SERIAL PRIMARY KEY,
+    notice_id INTEGER REFERENCES notices,
+    status STATUS,
+    set_by INTEGER REFERENCES users
 );
 
 CREATE TABLE polls (
@@ -73,14 +84,17 @@ CREATE TABLE polls (
     open_on TIMESTAMP,
     close_on TIMESTAMP,
     created_at TIMESTAMP,
-    created_by INTEGER REFERENCES users
+    created_by INTEGER REFERENCES users,
+    archived_at TIMESTAMP,
+    archived_by INTEGER REFERENCES users
 );
 
 CREATE TABLE votes (
     id SERIAL PRIMARY KEY,
     poll_id INTEGER REFERENCES polls,
-    thumbs_up_by INTEGER REFERENCES users,
-    thumbs_down_by INTEGER REFERENCES users
+    vote: BOOLEAN,
+    voted_at TIMESTAMP,
+    voted_by INTEGER REFERENCES users
 );
 
 CREATE TABLE feedbacks (
@@ -89,5 +103,8 @@ CREATE TABLE feedbacks (
     body VARCHAR(1000),
     sent_at TIMESTAMP,
     sent_by INTEGER REFERENCES users,
-    acknowledged BOOLEAN
+    acknowledged_at TIMESTAMP,
+    acknowledged_by INTEGER REFERENCES users,
+    archived_at TIMESTAMP,
+    archived_by INTEGER REFERENCES users
 );
